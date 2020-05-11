f3195ae7266389701a6c80520722eecaca5d40ad
mt <margarettom6@gmail.com>
2020-05-11 07:26:27 +0300

    Vue - components

---
+++ main.js
@@ -0,0 +1,32 @@
+Vue.components('blog-post', {
+    props: ['blog'],
+    template: `
+     <div class="blog article one">
+     <h2> {{blog.title}} </h2>
+     <h6>By {{blog.author}} on {{blog.date}} </h6>
+     <p> {{blog.content}}
+     </p>
+     </div>`
+});
+const blogs = [
+    {
+        id: 1,
+        title: 'Learning Javascript',
+        author: 'M-tee',
+        date: 'Thu apr 23 2020 10:43 GMT -00400 (Eastern Daylight Time)',
+        content: 'dolor  sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor.'
+    },
+    {
+        id: 2,
+        title: 'Learning Javascript',
+        author: 'M-tee',
+        date: 'Thu apr 23 2020 10:43 GMT -00400 (Eastern Daylight Time)',
+        content: 'dolor  sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor.'
+    }
+];
+new Vue({
+    el: '#blog-section',
+    data: {
+        blogs
+    }
+});
